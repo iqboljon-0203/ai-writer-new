50ad75153cdcb845e8c4cac3a904c18557b52cf6
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-11-18 15:51:18 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@/components/ui/button'
+function App() {
+    return (
+        <>
+            <div className="text-danger-400 bg-red-300">salom</div>
+            <Button variant={'outline'}>Click me</Button>
+        </>
+    )
+}
+
+export default App
